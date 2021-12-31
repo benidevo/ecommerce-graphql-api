2bc187e0d584d229bd9e1d8d5ea531e747b5f7aa
Benjamin Idewor <benjaminidewor@gmail.com>
2022-01-01 00:22:27 +0100

    add products query

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const { ApolloServer, gql } = require('apollo-server');
+const {products} = require('./data/products');
 
 const typeDefs = gql`
     type Query {
         hello: String
+        products: [Product]
+    }
+
+    type Product {
+        name: String
+        description: String
+        quantity: Int
+        image: String
+        price: Float
+        onSale: Boolean
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => 'Hello world!'
+        hello: () => 'Hello world!',
+        products: () => {
+            return products;
+        }
     }
 }
 
